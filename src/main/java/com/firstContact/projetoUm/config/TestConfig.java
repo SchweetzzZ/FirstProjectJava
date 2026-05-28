9686a45a48cdcb110f2c92e90b3aa5c54ec6049e
SchweetzzZ <jonatas_kp32@hotmail.com>
2026-05-28 09:38:48 -0300

    feat(DB): populate database

--- src/main/java/com/firstContact/projetoUm/config/TestConfig.java
+++ src/main/java/com/firstContact/projetoUm/config/TestConfig.java
@@ -1,10 +1,13 @@
 package com.firstContact.projetoUm.config;
 
+import com.firstContact.projetoUm.entity.User;
 import com.firstContact.projetoUm.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
+
+import java.util.Arrays;
 
 @Configuration
 @Profile("test")
@@ -14,7 +17,10 @@ public class TestConfig implements CommandLineRunner {
     private UserRepository userRepository;
 
     @Override
-    public void run(String... argds) throws Exception
+    public void run(String... args) throws Exception{
 
-
+        User u1 = new User(null, "Maria Brown", "maria@gmail.com", "988888888", "123456");
+        User u2 = new User(null, "Alex Green", "alex@gmail.com", "977777777", "123456");
+        userRepository.saveAll(Arrays.asList(u1,u2));
+    }
 }
