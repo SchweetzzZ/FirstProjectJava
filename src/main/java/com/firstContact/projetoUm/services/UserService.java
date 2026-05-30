49b12d82d127db689bd40956c7e0dfb2ed695184
Jonatas Caçula Soares <jonatas_kp32@hotmail.com>
2026-05-29 21:10:04 -0300

    feat: tratando alguns erros globais para teste

--- src/main/java/com/firstContact/projetoUm/services/UserService.java
+++ src/main/java/com/firstContact/projetoUm/services/UserService.java
@@ -2,6 +2,7 @@ package com.firstContact.projetoUm.services;
 
 import com.firstContact.projetoUm.entity.User;
 import com.firstContact.projetoUm.repositories.UserRepository;
+import com.firstContact.projetoUm.services.exceptions.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -17,8 +18,33 @@ public class UserService {
     public List<User> findAll() {
         return repository.findAll();
     }
+
     public User findById(Long id) {
         Optional<User> obj = repository.findById(id);
-        return obj.get();
+        return obj.orElseThrow(() -> new ResourceNotFoundException((id)));
+    }
+
+    public User inser(User obj){
+        return repository.save(obj);
+    }
+
+    public void delet(Long id){
+        try {
+            repository.deleteById(id);
+        } catch(RuntimeException e){
+            e.printStackTrace();
+        }
+    }
+
+    public User update(Long id, User obj) {
+        User entity = repository.getReferenceById(id);
+        updateData(entity, obj);
+        return repository.save(entity);
+    }
+
+    private void updateData(User entity, User obj) {
+        entity.setName(obj.getName());
+        entity.setEmail(obj.getEmail());
+        entity.setPhone(obj.getPhone());
     }
 }
