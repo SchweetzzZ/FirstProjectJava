375a3a25851bbc2257beb3ff95a5abd9552d45d5
SchweetzzZ <jonatas_kp32@hotmail.com>
2026-06-02 11:56:01 -0300

    feat(CommentsEntity): create comments entity

---
+++ src/main/java/com/firstContact/projetoUm/entity/Comments.java
@@ -0,0 +1,96 @@
+package com.firstContact.projetoUm.entity;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import jakarta.persistence.*;
+
+import java.io.Serializable;
+import java.time.Instant;
+import java.util.HashSet;
+import java.util.Objects;
+import java.util.Set;
+
+@Entity
+@Table(name = "tb_comments")
+public class Comments implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(columnDefinition = "TEXT")
+    private String content;
+
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss'Z'", timezone = "GMT")
+    private Instant moment;
+
+    @ManyToOne
+    @JoinTable(name = "client.id")
+    private User client;
+
+    @ManyToOne
+    @JoinColumn(name = "product.id")
+    private Product products;
+
+    public Comments(){};
+
+    public Comments(Long id, String content, Instant moment, User client, Product product) {
+        this.id = id;
+        this.content = content;
+        this.moment = moment;
+        this.client = client;
+        this.products = product;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public Instant getMoment() {
+        return moment;
+    }
+
+    public void setMoment(Instant moment) {
+        this.moment = moment;
+    }
+
+    public User getClient() {
+        return client;
+    }
+
+    public void setClient(User client) {
+        this.client = client;
+    }
+
+    public Product getProducts() {
+        return products;
+    }
+
+    public void setProducts(Product products) {
+        this.products = products;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Comments comments = (Comments) o;
+        return Objects.equals(getId(), comments.getId());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(getId());
+    }
+}
